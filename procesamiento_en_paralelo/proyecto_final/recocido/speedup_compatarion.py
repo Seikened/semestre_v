ec04146b5116d76aa860de63b3ec641e82fc8b8d
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-12-02 14:22:52 -0600

    feat: reestructurar el código de Evolución Diferencial y comparación de speedup
    - Se eliminó el archivo 'paralelizado.py' y se reimplementó con una nueva estructura.
    - Se eliminó el archivo 'proyectofinal_evoluciondiferencial_recocidosimulado.py' y se reescribió con nuevas funciones.
    - Se eliminó el archivo 'speedup_compatarion.py' y se creó una nueva versión para comparar el rendimiento entre algoritmos paralelizados y no paralelizados.

---
+++ procesamiento_en_paralelo/proyecto_final/recocido/speedup_compatarion.py
@@ -0,0 +1,26 @@
+from paralelizado import main_paralelizado
+from proyectofinal_evoluciondiferencial_recocidosimulado import main_no_paralelizado
+import matplotlib.pyplot as plt
+
+
+lis_gen = [100, 1_000, 10_000]
+
+for gen in lis_gen:
+    pso_no_paralelizado, lista_tiempos_no_paralelizado = main_no_paralelizado(gen)
+    pso_paralelizado, lista_tiempos_paralelizado = main_paralelizado(gen)
+
+    speedup = pso_no_paralelizado / pso_paralelizado
+    print(f"Speedup: {speedup:.2f}x")
+
+
+    # Gráfica de comparación de tiempos por generación
+    plt.figure(figsize=(10, 6))
+    plt.plot(lista_tiempos_no_paralelizado, label='No Paralelizado', alpha=0.7)
+    plt.plot(lista_tiempos_paralelizado, label='Paralelizado', alpha=0.7)
+    plt.xlabel('Generación')
+    plt.ylabel('Tiempo por generación (segundos)')
+    plt.title(f'Comparación de Tiempos por Generación, speedup: {speedup:.2f}x')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
